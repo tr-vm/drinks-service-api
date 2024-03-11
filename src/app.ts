f1c3c91d4b942ddbe63536eedbd8affd03d6ab89
Victor Matekole <victor@matekole.com>
2024-03-11 20:58:46 +0100

    feat: add coffee_cotroller

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import express from "express";
 export const app = express();
+app.use(express.json());
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
+app.get("/coffee", (req, res) =>
+  res.json({
+    drinkType: "Coffee",
+    name: "Latte",
+  }),
+);
